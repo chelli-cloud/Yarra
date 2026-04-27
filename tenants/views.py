50d06b7da8d6400beeba7d6d5ae2509c81479e4d
Vishnu Aravindh <rvishnu23@gmail.com>
2026-04-27 12:17:09 +0530

    Fix NameError: import User in tenants/views.py

--- tenants/views.py
+++ tenants/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import authenticate, login, logout
